3ed627d2503ecebc9904513bdeb04489e8153e33
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-04-17 09:58:23 +0530

    Add files via upload

---
+++ crazymind/vars.py
@@ -0,0 +1,5 @@
+api_id = 26181533
+api_hash = "61f8a38f9d9d079100ba56fcbb9ea9b7"
+bot_token = "6678551865:AAEEsJPFAwN74V2bH7Xe6e2nKAxTVIrcejo"
+AUTH = 6329158981
+#Bot Username = "Dhdjowtiknddtndwudbot"
