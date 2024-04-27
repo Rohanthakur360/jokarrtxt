18294c7c7dcf1b03d0a8e1a57bc7a89ee3238e1d
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-04-27 19:42:46 +0530

    Update vars.py

--- crazymind/vars.py
+++ crazymind/vars.py
@@ -1,5 +1,5 @@
 api_id = 26181533
 api_hash = "61f8a38f9d9d079100ba56fcbb9ea9b7"
-bot_token = "6678551865:AAFptVg2n2DqVoY2KhMehdR5HUNuDj0BUkc"
+bot_token = "6706535540:AAF27SlXPmgl2NZtC0lP_Ql9ZtPf7nMBrSk"
 AUTH = 6329158981
-#Bot Username = "Dhdjowtiknddtndwudbot"
+#Bot Username = @bddbvhdhhdvddbot""
